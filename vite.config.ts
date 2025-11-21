0bd93d99dbb0bc034282c68a596fb8c62b0de189
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2025-11-21 22:37:43 +0100

    ci: Add GitHub Pages deployment workflow and configure Vite base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/maze",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
