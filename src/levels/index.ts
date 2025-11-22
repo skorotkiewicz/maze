6728920d5d627450726c0d14dc5a1fe800d86f68
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2025-11-22 03:29:58 +0100

    feat: add new level 'levelx'

--- src/levels/index.ts
+++ src/levels/index.ts
@@ -1,12 +1,12 @@
 import type { Level } from "../types";
-
 import level1 from "./level1.json";
 import level2 from "./level2.json";
 import level3 from "./level3.json";
 import level4 from "./level4.json";
+import levelx from "./levelx.json";
 // import level5 from "./level5.json";
 // import level6 from "./level6.json";
 
-export const LEVELS: Level[] = [level1, level2, level3, level4];
+export const LEVELS: Level[] = [level1, level2, level3, level4, levelx];
 
 export type { Level };
